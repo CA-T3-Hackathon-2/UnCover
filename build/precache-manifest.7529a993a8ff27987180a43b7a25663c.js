84656224584c299489b78fd81d266b49b5c8378c
chivoi <a.lastovirya@gmail.com>
2021-06-08 18:25:39 +1000

    creates build, deploys app

---
+++ build/precache-manifest.7529a993a8ff27987180a43b7a25663c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120c89b797f4a7daf9914a4e882e6744",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ccdc66240eb3a2d8a84",
+    "url": "/static/js/2.a55263ce.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a55263ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a23a8ce025e1086134f",
+    "url": "/static/js/main.1ccb3c6a.chunk.js"
+  },
+  {
+    "revision": "d9ace9ea230f0fbac147",
+    "url": "/static/js/runtime-main.53c59ca3.js"
+  }
+]);
