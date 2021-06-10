0b7ac3f6d188be074b29f7f893d31cc15ba38616
chivoi <a.lastovirya@gmail.com>
2021-06-10 17:21:49 +1000

    refactors form with styled components

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,55 +1,45 @@
 import React from "react";
-import { categoryIds, dateFormatted } from "../utils/helpers";
-import Submit from "./styled/Submit";
-import capitalize from "../utils/capitalize";
 import { Link } from "react-router-dom";
+// data
+import { categoryIds, dateFormatted } from "../utils/helpers";
+// utils
+import capitalize from "../utils/capitalize";
+// styled components
+import Submit from "./styled/Submit";
+import FormDiv from "./styled/form/FormDiv";
+import FormHeading from "./styled/form/FormHeading";
+import StyledForm from "./styled/form/StyledForm";
+import FormHorizontalDiv from "./styled/form/FormHorizontalDiv";
+import FormVerticalDiv from "./styled/form/FormVerticalDiv";
+
 
 const Form = ({ formData, dispatch }) => {
   const categories = Object.keys(categoryIds);
   const { category, dateFrom, dateTo, locationDistance, price } = formData;
 
-  const formStyles = {
-    display: "flex",
-    flexDirection: "column",
-    margin: "3em",
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
+    // hit the API
   };
 
   return (
-    <div style={{ width: "75rem" }}>
-      <h2 style={{ textAlign: "center" }}>Find your next event</h2>
-      <form style={formStyles} onChange={handleSubmit}>
+    <FormDiv>
+      <FormHeading>Find your next event</FormHeading>
+      <StyledForm>
         <label htmlFor="category">Event Category</label>
         <select
           onChange={(e) =>
             dispatch({ type: "setCategory", data: e.target.value })
           }
-          value={category}
-        >
+          value={category}>
           {categories.map((category) => {
             return <option key={category}>{capitalize(category)}</option>;
           })}
         </select>
 
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginTop: "1rem",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              marginRight: "1rem",
-            }}
-          >
+        // col 1
+        <FormHorizontalDiv>
+          <FormVerticalDiv style={{marginRight: "1rem"} }>
             <label htmlFor="datemin">Date From</label>
             <input
               type="date"
@@ -61,15 +51,8 @@ const Form = ({ formData, dispatch }) => {
               }
               min={dateFormatted(new Date())}
             />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              marginLeft: "1rem",
-            }}
-          >
+          </FormVerticalDiv>
+          <FormVerticalDiv style={{marginLeft: "1rem"} }>
             <label htmlFor="datemax">Date To</label>
             <input
               type="date"
@@ -81,25 +64,12 @@ const Form = ({ formData, dispatch }) => {
               }
               min={dateFormatted(new Date())}
             />
-          </div>
-        </div>
-
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginTop: "1rem",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              marginRight: "1rem",
-            }}
-          >
+          </FormVerticalDiv>
+        </FormHorizontalDiv>
+        
+        // col 2
+        <FormHorizontalDiv>
+          <FormVerticalDiv style={{marginRight: "1rem"}}>
             <label htmlFor="locationDistance">Location Distance (km)</label>
             <input
               type="number"
@@ -108,15 +78,8 @@ const Form = ({ formData, dispatch }) => {
               }
               value={locationDistance}
             />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              marginLeft: "1rem",
-            }}
-          >
+          </FormVerticalDiv>
+          <FormVerticalDiv style={{marginLeft: "1rem"}}>
             <label htmlFor="price">Max price: $</label>
             <input
               type="number"
@@ -125,13 +88,13 @@ const Form = ({ formData, dispatch }) => {
               }
               value={price}
             />
-          </div>
-        </div>
+          </FormVerticalDiv>
+        </FormHorizontalDiv>
         <Link to="/results">
           <Submit>Submit</Submit>
         </Link>
-      </form>
-    </div>
+      </StyledForm>
+    </FormDiv>
   );
 };
 
