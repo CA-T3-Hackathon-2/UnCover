1f2f59e440dbb2c1017a8faed8fb72c65b0a96a6
Rhys Morris <rhysmorris08@gmail.com>
2021-06-10 22:47:27 +1000

    Results style changes

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,27 +1,42 @@
 import React from "react";
 import Map from "./Map";
 import ResultItem from "./ResultItem";
+import { Link } from "react-router-dom";
 
 const Results = (props) => {
   console.log(props);
 
-
   return (
-    <div style={{ display: "flex" }}>
-      <div
+    <section style={{ padding: "5rem" }}>
+      <p
         style={{
-          flex: "0.5",
-          padding: "10px",
-          overflowY: "scroll",
-          height: "80vh",
+          alignSelf: "flex-start",
+          paddingLeft: "3rem",
+          fontSize: "2rem",
         }}
       >
-        <ResultItem />
+        <Link to="/find" style={{ textDecoration: "none", color: "inherit" }}>
+          Back
+        </Link>{" "}
+        > Results
+      </p>
+      <div style={{ display: "flex" }}>
+        <div
+          style={{
+            flex: "0.4",
+            padding: "3rem",
+            overflowY: "scroll",
+            height: "80vh",
+            marginRight: "3rem",
+          }}
+        >
+          <ResultItem />
+        </div>
+        <div style={{ flex: "0.6", margin: "10px", overflow: "hidden" }}>
+          <Map />
+        </div>
       </div>
-      <div style={{ flex: "0.5", margin: "10px", overflow: "hidden" }}>
-        <Map />
-      </div>
-    </div>
+    </section>
   );
 };
 
---
+++ client/src/components/styled/results/EventDate.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const EventDate = styled.span`
+  font-size: 1.6rem;
+  font-weight: 400;
+`;
+
+export default EventDate;
