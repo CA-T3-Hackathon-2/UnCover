55cdf9f5a3af685ebc14199c63b0607d13ff3aa8
Rhys Morris <rhysmorris08@gmail.com>
2021-06-10 16:04:43 +1000

    Basic styling form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -19,7 +19,7 @@ const Form = ({ formData, dispatch }) => {
   };
 
   return (
-    <div style={{ width: "80rem" }}>
+    <div style={{ width: "75rem" }}>
       <h2 style={{ textAlign: "center" }}>Find your next event</h2>
       <form style={formStyles} onChange={handleSubmit}>
         <label htmlFor="category">Event Category</label>
@@ -39,51 +39,93 @@ const Form = ({ formData, dispatch }) => {
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
-            margin: "20px 0",
+            marginTop: "1rem",
           }}
         >
-          <label htmlFor="datemin">Date From</label>
-          <input
-            type="date"
-            id="datemin"
-            name="datemin"
-            value={dateFrom}
-            onChange={(e) =>
-              dispatch({ type: "setDateFrom", data: e.target.value })
-            }
-            min={dateFormatted(new Date())}
-          />
-          <label htmlFor="datemax">Date To</label>
-          <input
-            type="date"
-            id="datemax"
-            name="datemax"
-            value={dateTo}
-            onChange={(e) =>
-              dispatch({ type: "setDateTo", data: e.target.value })
-            }
-            min={dateFormatted(new Date())}
-          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              marginRight: "1rem",
+            }}
+          >
+            <label htmlFor="datemin">Date From</label>
+            <input
+              type="date"
+              id="datemin"
+              name="datemin"
+              value={dateFrom}
+              onChange={(e) =>
+                dispatch({ type: "setDateFrom", data: e.target.value })
+              }
+              min={dateFormatted(new Date())}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              marginLeft: "1rem",
+            }}
+          >
+            <label htmlFor="datemax">Date To</label>
+            <input
+              type="date"
+              id="datemax"
+              name="datemax"
+              value={dateTo}
+              onChange={(e) =>
+                dispatch({ type: "setDateTo", data: e.target.value })
+              }
+              min={dateFormatted(new Date())}
+            />
+          </div>
         </div>
 
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          <label htmlFor="locationDistance">Location Distance (km)</label>
-          <input
-            type="number"
-            onChange={(e) =>
-              dispatch({ type: "setLocationDistance", data: e.target.value })
-            }
-            value={locationDistance}
-          ></input>
-
-          <label htmlFor="price">Max price: $</label>
-          <input
-            type="number"
-            onChange={(e) =>
-              dispatch({ type: "setPrice", data: e.target.value })
-            }
-            value={price}
-          ></input>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: "1rem",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              marginRight: "1rem",
+            }}
+          >
+            <label htmlFor="locationDistance">Location Distance (km)</label>
+            <input
+              type="number"
+              onChange={(e) =>
+                dispatch({ type: "setLocationDistance", data: e.target.value })
+              }
+              value={locationDistance}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              marginLeft: "1rem",
+            }}
+          >
+            <label htmlFor="price">Max price: $</label>
+            <input
+              type="number"
+              onChange={(e) =>
+                dispatch({ type: "setPrice", data: e.target.value })
+              }
+              value={price}
+            />
+          </div>
         </div>
         <Link to="/results">
           <Submit>Submit</Submit>
