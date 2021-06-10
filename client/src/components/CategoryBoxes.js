88ba7b5bea00441fbd396a859dc28acc1e745616
irah rosete <irah.rosete@outlook.com>
2021-06-10 15:40:05 +1000

    update CategoryBoxes

--- client/src/components/CategoryBoxes.js
+++ client/src/components/CategoryBoxes.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
-const CategoryBox = ({ category, setCategory }) => {
-  
-  const boxesSectionStyles = {
+const CategoryBoxes = () => {
+  const categories = [
+    {
+      id: 1,
+      description: "Arts"
+    },
+    {
+      id: 3,
+      description: "Education"
+    },
+    {
+      id: 6,
+      description: "Music"
+    },
+    {
+      id: 7,
+      description: "Sports"
+    },
+    {
+      id: 11,
+      description: "Exhibitions"
+    },
+    {
+      id: 190,
+      description: "Lifestyle"
+    },
+    {
+      id: 246,
+      description: "All"
+    },
+  ]
+
+  const sectionBoxesStyles = {
     display: "flex",
     flexDirection: "row"
   }
@@ -11,16 +41,20 @@ const CategoryBox = ({ category, setCategory }) => {
     display: "flex",
     flexDirection: "column",
     border: "1px solid black",
+    borderRadius: "5px",
+    margin: "1rem",
+    padding: "1rem"
   }
 
   return(
-    <div>
-      <div >
-        <p>Hello I render</p>
-        {category}
+    <div style={sectionBoxesStyles}>
+      <div>
+        {categories.map((category) => (
+          <p style={categoryBoxStyles} key={category.id}>{category.description}</p>
+        ))}
       </div>
     </div>
   )
 }
 
-export default CategoryBox;
+export default CategoryBoxes;
