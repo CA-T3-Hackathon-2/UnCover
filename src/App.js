948ad9e7e308fd828c81bf85a86bed024eff6770
Darko Mihajlovic <66908757+DarkoAU@users.noreply.github.com>
2021-06-10 13:46:01 +1000

    Revert "Results page"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Results from './components/Results'
 
 const App = () => {
 
 	return (
 		<>
 			<h1 style={{textAlign: 'center' }}>UnCover</h1>
-			<Results />
 		</>
 	);
 };
