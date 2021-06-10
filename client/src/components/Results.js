a96e1bd5f6977eaab4edbd06edde3e818e3a7f66
DarkoAU <darko.mihajlovic.22@gmail.com>
2021-06-10 13:16:55 +1000

    adds correct ResultItem child component to Resuls component

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Map from './Map'
-import * as eventData from '../eventData.json'
+import ResultItem from './ResultItem'
 
 const Results = () => {
 
@@ -8,13 +8,7 @@ const Results = () => {
 
             <div style={{display: 'flex'}}>
                 <div style={{flex: '0.5', padding: '10px', overflowY: 'scroll', height: '80vh'}}>
-                    {eventData.events.map((targetEvent) => (
-                        <div>
-                            <h5><a href={targetEvent.url} rel="noopener noreferrer" target="_blank">{targetEvent.name}</a></h5>
-                            <p>{targetEvent.description}</p>
-                            <h6>{targetEvent.location_summary}</h6>
-                        </div>
-                ))}
+                    <ResultItem />
                 </div>
                 <div style={{flex: '0.5', margin: '10px',  overflow: 'hidden'}}>
                     <Map />
