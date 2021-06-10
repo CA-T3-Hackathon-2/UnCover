ee0d7e845d686e8695c8a1cc6acaf128b6325109
chivoi <a.lastovirya@gmail.com>
2021-06-10 13:56:35 +1000

    creates Find component

---
+++ client/src/components/Find.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Form from './Form'
+import CategoryBox from './CategoryBox'
+
+const Find = (props) => {
+
+  return(
+    <>
+      <Form />
+      <CategoryBoxes />
+    </>
+  )
+
+}
+
+export default Find;
